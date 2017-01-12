b64a9588f39d2581a6e097dfb73817054c0a0370
geniusninedev <developer@nineinfosys.com>
2017-01-12 17:10:23 +0530

    Login_Activity and splash screen and drable images

--- app/src/main/java/com/geniusnine/android/geniusninelifecare/Login_Patient/Patient_Login.java
+++ app/src/main/java/com/geniusnine/android/geniusninelifecare/Login_Patient/Patient_Login.java
@@ -1,9 +1,17 @@
 package com.geniusnine.android.geniusninelifecare.Login_Patient;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.geniusnine.android.geniusninelifecare.Helper.DBHelper;
+import com.geniusnine.android.geniusninelifecare.MainActivityDrawer;
+import com.geniusnine.android.geniusninelifecare.Patient_Registration.Patient_Registration;
 import com.geniusnine.android.geniusninelifecare.R;
 
 /**
@@ -12,10 +20,43 @@ import com.geniusnine.android.geniusninelifecare.R;
 
 public class Patient_Login extends Activity {
     DBHelper dbHelper;
+    EditText edittextUsername,edittextPassword;
+    TextView textViewforgetpassword;
+    Button buttonsignIn,buttonsignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.patient_login);
+        edittextUsername=(EditText)findViewById(R.id.editTextUserName);
+        edittextPassword=(EditText)findViewById(R.id.editTextPassword);
+        textViewforgetpassword=(TextView)findViewById(R.id.textViewforgetpasswordclick);
+        buttonsignIn=(Button)findViewById(R.id.buttonSignIn);
+        buttonsignUp=(Button)findViewById(R.id.buttonSignUp);
+        buttonsignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i1=new Intent(Patient_Login.this, MainActivityDrawer.class);
+                startActivity(i1);
+            }
+        });
+        buttonsignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i2=new Intent(Patient_Login.this, Patient_Registration.class);
+                startActivity(i2);
+
+            }
+        });
+        textViewforgetpassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Patient_Login.this,"Forget Password",Toast.LENGTH_SHORT).show();
+
+            }
+        });
     }
-}
+
+
+
+    }
