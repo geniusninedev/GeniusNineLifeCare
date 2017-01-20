da5bcbbac6b37fda06d55ce72ea133f418654e73
geniusninedev <genius9>
2017-01-20 10:50:55 +0530

    New Health and Tips Model Added In Project

---
+++ app/src/main/java/com/geniusnine/android/geniusninelifecare/Fragments/Add_Health_and_Tips.java
@@ -0,0 +1,30 @@
+package com.geniusnine.android.geniusninelifecare.Fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.geniusnine.android.geniusninelifecare.R;
+
+;
+
+
+/**
+ * Created by Dev on 12-01-2017.
+ */
+
+public class Add_Health_and_Tips extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.add_helth_and_tips, null);
+
+
+
+
+        return v;
+    }
+
+}
